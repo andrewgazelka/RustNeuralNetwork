8a7ad31feb1333aaff58e76caeb458a02be4f87d
Andrew Gazelka <andrew.gazelka@gmail.com>
2020-12-17 15:55:29 -0600

    add column view for numbers

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io;
 
 use clap::Clap;
+use itertools::Itertools;
 
 use crate::files::mnist::MNIST;
 
@@ -23,23 +24,68 @@ enum SubCommand {
 #[derive(Clap)]
 struct Numbers {
     #[clap(short, long)]
-    amount: Option<usize>
+    height: Option<usize>,
 
+    #[clap(short, long)]
+    columns: Option<usize>,
 }
 
 fn main() -> Result<(), io::Error> {
     let opts: Opts = Opts::parse();
+
     match opts.subcmd {
-        SubCommand::Numbers(n) => gen_numbers(n.amount.unwrap_or(usize::max_value()))
+        SubCommand::Numbers(n) => gen_numbers(
+            n.height.unwrap_or(usize::max_value()),
+            n.columns.unwrap_or(3),
+        )
     }?;
 
     Ok(())
 }
 
-fn gen_numbers(amount: usize) -> Result<(), io::Error> {
+fn gen_numbers(height: usize, columns: usize) -> Result<(), io::Error> {
     let mnist = MNIST::new("data/train-labels", "data/train-images")?;
-    for x in mnist.images.iter().take(amount) {
-        println!("{}", x.to_string())
+
+    for images in mnist.images.chunks(columns).take(height) {
+        let numbers = images.iter().map(|image| image.to_string()).collect_vec();
+        let mut line_iterators = Vec::new();
+
+        for number in &numbers {
+            line_iterators.push(number.lines());
+        }
+
+        loop {
+            let mut has_one = false;
+            for lines in &mut line_iterators {
+                if let Some(line) = lines.next() {
+                    has_one = true;
+                    print!("{}", line);
+                }
+            }
+            println!();
+            if !has_one {
+                break;
+            }
+        }
+
+        // let lines  = images.iter().map(|image| {
+        //     let x = image.to_string().lines();
+        //     x
+        // });
+
+
+        //
+        // for a in gp {
+        //
+        // }
+
+
+        // .group_by(|(i, str)| i);
+        // .group_by(|(i, line)| i);
+        // .group_by(|(i, s)| 1);
+
+
+        // let lines = images.iter().map(|image| image.to_string().lines().co);
     }
     Ok(())
 }
